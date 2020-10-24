d1d64b9007af5801fbc47d537bca196031574606
Eduardo Andrade Santos <eandrasa@everis.com>
2020-10-24 15:46:04 -0300

    first commit

---
+++ src/LogicaUsada.java
@@ -0,0 +1,24 @@
+/*
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class LogicaUsada {
+
+	public static void main(String[] args) {
+
+		Scanner leitor = new Scanner(System.in);
+		int[] array = new int[10]; // cria 10 posições
+		int contador = 0; // contador inicia com 0
+
+		while (contador < 10) {
+			System.out.println("Insira um numero: ");
+			array[contador] = leitor.nextInt(); // guarda o valor na posição
+			contador++; // contador recebe +1
+		}
+
+		Arrays.sort(array); // organiza valores do array em ordem
+		System.out.println(Arrays.toString(array)); // mostra o array em ordem
+	}
+
+}
+*/
---
+++ src/Ordenador.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Ordenador {
+	Scanner leitor = new Scanner(System.in);
+	public int[] Array = new int[10]; // 10 posições
+	public int Contador = 0;
+
+	public Ordenador() {
+		insereNumeros();
+		organizaResultado();
+		exibeOrdem();
+	}
+
+	public void insereNumeros() {
+		while (Contador < 10) {
+			System.out.println("Insira um numero: ");
+			Array[Contador] = leitor.nextInt(); // guarda o valor na posição
+			Contador++; // contador recebe +1
+		}
+	}
+
+	public void organizaResultado() {
+		Arrays.sort(Array); // organiza valores do array em ordem
+	}
+
+	public void exibeOrdem() {
+		System.out.println(Arrays.toString(Array)); // mostra o array em ordem
+	}
+}
